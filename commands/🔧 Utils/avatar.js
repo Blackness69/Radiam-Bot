a717f9e21b963615de87cdcfa64d88f71410ad0d
Blackness69 <161008491+Blackness69@users.noreply.github.com>
2024-05-17 06:28:03 +0000

    .

---
+++ commands/🔧 Utils/avatar.js
@@ -0,0 +1,27 @@
+const {  EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+
+module.exports = {
+        name: 'avatar',
+        aliases: ['av'],
+        description: 'Get your own avatar or the avatar of another user',
+    async execute({msg}) {
+        const user = msg.mentions.members.first() || msg.author;
+
+        const link = new ActionRowBuilder()
+            .addComponents(
+                new ButtonBuilder()
+                    .setLabel('Avatar Link')
+                    .setURL(user.displayAvatarURL({ format: "png", size: 2048 }))
+                    .setStyle(ButtonStyle.Link)
+            );
+
+        const avatar = new EmbedBuilder()
+            .setDescription(`Avatar of **${user.username}**`)
+            .setFooter({ text: `${interaction.guild.name}`, iconURL: interaction.client.user.displayAvatarURL({ dynamic: true }) })
+            .setImage(user.displayAvatarURL({ format: "png", size: 2048 }))
+            .setTimestamp()
+            .setColor('#A020F0');
+
+        await msg.reply({ embeds: [avatar], components: [link] });
+    },
+};
