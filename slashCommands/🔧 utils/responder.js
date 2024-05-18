e95d16cd2ba950fac8e1ffc047f2bd8ea8ec6696
Blackness69 <161008491+Blackness69@users.noreply.github.com>
2024-05-18 08:35:08 +0000

    .

--- slashCommands/🔧 utils/responder.js
+++ slashCommands/🔧 utils/responder.js
@@ -48,8 +48,8 @@ module.exports = {
     const subcommand = interaction.options.getSubcommand();
 
     if (subcommand === 'add') {
-      const trigger = interaction.options.getString('trigger');
-      const response = interaction.options.getString('response');
+      const trigger = interaction.options.getString('trigger').toLowerCase();
+      const response = interaction.options.getString('response').toLowerCase();
 
       const data = await schema.findOne({ guildId: interaction.guild.id });
 
@@ -58,64 +58,54 @@ module.exports = {
           guildId: interaction.guild.id,
           autoresponses: [
             {
-              trigger: trigger,
-              response: response,
+              trigger,
+              response,
             }
           ]
         });
-
-        const embed = new EmbedBuilder()
-          .setTitle('AutoResponse Created')
-          .setDescription(`Trigger:\n${trigger}\n\nResponse: ${response}`)
-          .setTimestamp();
-
-        await interaction.reply({ embeds: [embed] });
       } else {
         const autoresponders = data.autoresponses;
         for (const t of autoresponders) {
-          if (t.trigger === trigger)
+          if (t.trigger === trigger) {
             return interaction.reply({
               content: "You must have unique triggers.",
               ephemeral: true
             });
+          }
         }
-
-        const addto = {
-          trigger: trigger,
-          response: response,
-        };
 
         await schema.findOneAndUpdate(
           { guildId: interaction.guild.id },
-          { $push: { autoresponses: addto } }
+          { $push: { autoresponses: { trigger, response } } }
         );
-
-        const embed = new EmbedBuilder()
-          .setTitle('AutoResponse Created')
-          .setDescription(`\nTrigger: ${trigger}\n\nResponse: ${response}`)
-          .setTimestamp();
-
-        await interaction.reply({ embeds: [embed] });
       }
+
+      const embed = new EmbedBuilder()
+        .setTitle('AutoResponse Created')
+        .setDescription(`Trigger: ${trigger}\nResponse: ${response}`)
+        .setTimestamp();
+
+      await interaction.reply({ embeds: [embed] });
+
     } else if (subcommand === 'remove') {
-      const trigger = interaction.options.getString('trigger');
+      const trigger = interaction.options.getString('trigger').toLowerCase();
+
       const data = await schema.findOneAndUpdate(
         { guildId: interaction.guild.id },
-        { $pull: { autoresponses: { trigger: trigger } } },
+        { $pull: { autoresponses: { trigger } } },
         { new: true }
       );
 
-      if (!data) {
+      if (!data || !data.autoresponses.some(a => a.trigger === trigger)) {
         const embed = new EmbedBuilder()
           .setDescription('I couldn\'t find an auto response with that trigger.');
-
-        await interaction.reply({ embeds: [embed], ephemeral: true });
-      } else {
-        const embed = new EmbedBuilder()
-          .setDescription(`Deleted that auto response\n\nTrigger: ${trigger}`);
-
-        await interaction.reply({ embeds: [embed] });
+        return interaction.reply({ embeds: [embed], ephemeral: true });
       }
+
+      const embed = new EmbedBuilder()
+        .setDescription(`Deleted auto response with trigger: ${trigger}`);
+      await interaction.reply({ embeds: [embed] });
+
     } else if (subcommand === 'list') {
       const data = await schema.findOne({ guildId: interaction.guild.id });
 
@@ -123,38 +113,36 @@ module.exports = {
         const embed = new EmbedBuilder()
           .setTitle('AutoResponse List')
           .setDescription('No autoresponses found.');
-
-        await interaction.reply({ embeds: [embed] });
-      } else {
-        const embed = new EmbedBuilder()
-          .setTitle('Autoresponse List')
-          .setDescription('List of all autoresponses.');
-
-        data.autoresponses.forEach((autoresponse, index) => {
-          embed.addFields({
-            name: `Autoresponse #${index + 1}`,
-            value: `Trigger: ${autoresponse.trigger}\nResponse: ${autoresponse.response}`,
-          });
-        });
-
-        await interaction.reply({ embeds: [embed] });
+        return interaction.reply({ embeds: [embed] });
       }
+
+      const embed = new EmbedBuilder()
+        .setTitle('AutoResponse List')
+        .setDescription('List of all autoresponses.');
+
+      data.autoresponses.forEach((autoresponse, index) => {
+        embed.addFields({
+          name: `Autoresponse #${index + 1}`,
+          value: `Trigger: ${autoresponse.trigger}\nResponse: ${autoresponse.response}`,
+        });
+      });
+
+      await interaction.reply({ embeds: [embed] });
+
     } else if (subcommand === 'remove-all') {
       const data = await schema.findOne({ guildId: interaction.guild.id });
 
       if (!data) {
         const embed = new EmbedBuilder()
           .setDescription('No Autoresponder Found.');
-
-        return await interaction.reply({ embeds: [embed], ephemeral: true });
-      } else {
-        await schema.deleteMany({ guildId: interaction.guild.id });
-
-        const embed = new EmbedBuilder()
-          .setDescription('Successfully deleted all responses.');
-
-        await interaction.reply({ embeds: [embed] });
+        return interaction.reply({ embeds: [embed], ephemeral: true });
       }
+
+      await schema.deleteMany({ guildId: interaction.guild.id });
+
+      const embed = new EmbedBuilder()
+        .setDescription('Successfully deleted all responses.');
+      await interaction.reply({ embeds: [embed] });
     }
   }
 };
