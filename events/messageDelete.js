94987ed8335630f06f1587ab39a6e4af5253e197
Blackness69 <161008491+Blackness69@users.noreply.github.com>
2024-06-14 04:28:59 +0000

    .

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,9 +1,12 @@
 const Discord = require('discord.js');
 const ms = require('ms');
-const client = require(process.cwd() + '/index.js')
+const client = require(process.cwd() + '/index.js');
 const { snipes } = require('../index.js');
 
 client.on("messageDelete", async (msg) => {
+  // Check if msg.author exists
+  if (!msg.author) return;
+
   // Add message delete event handling logic here
   const userTimestamps = client.messageTimestamps.get(msg.author.id) || [];
   const index = userTimestamps.indexOf(msg.createdTimestamp);
@@ -11,9 +14,8 @@ client.on("messageDelete", async (msg) => {
     userTimestamps.splice(index, 1);
     client.messageTimestamps.set(msg.author.id, userTimestamps);
   }
-  
-// snipe command
-  
+
+  // snipe command
   client.snipes.set(msg.channel.id, {
     content: msg.content,
     author: msg.author,
@@ -21,4 +23,4 @@ client.on("messageDelete", async (msg) => {
     avatarURL: msg.author.displayAvatarURL({ format: 'png', dynamic: true }),
     timestamp: msg.createdTimestamp
   });
-}); 
+});
